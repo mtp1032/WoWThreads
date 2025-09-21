5eb3ccdeb91d330f32dd7b91d3723d3a9421cb34
Michael Peterson <mtpeterson1948@gmail.com>
2025-09-21 09:29:58 -0600

    Update OptionsPanel.lua
    Added check buttons and addon descriptive text.

--- Sources/OptionsPanel.lua
+++ Sources/OptionsPanel.lua
@@ -1,41 +1,31 @@
+-- OptionsPanel.lua
+
 WoWThreads = WoWThreads or {}
 WoWThreads.OptionsPanel = WoWThreads.OptionsPanel or {}
 
 if not WoWThreads.Mgmt.loaded then
-    DEFAULT_CHAT_FRAME:AddMessage( "WoWThreadsMgmt.lua not loaded.", 1, 0, 0)
+    DEFAULT_CHAT_FRAME:AddMessage("WoWThreadsMgmt.lua not loaded.", 1, 0, 0)
     return
 end
 
-local thread = WoWThreads.ThreadLib
-
+local addonName = WoWThreads.Core:getAddonInfo()
+local thread = LibStub("WoWThreads")
+local utils = WoWThreads.UtilsLib
 local options = WoWThreads.OptionsPanel
-
 local L = WoWThreads.Locales.L
 
-local WIDTH_TITLE_BAR = 500
+local WIDTH_TITLE_BAR = 590 -- Match FRAME_WIDTH
 local HEIGHT_TITLE_BAR = 45
 local FRAME_WIDTH = 590
 local FRAME_HEIGHT = 400
 
-local DEFAULT_XPOS = 0
-local DEFAULT_YPOS = 200
-
-local enableDebugging = false
-local enableDataCollection = false
---------------------------------------------------------------------------
---                         CREATE THE VARIOUS BUTTONS
---------------------------------------------------------------------------
-local function createDefaultsButton(f)
-    -- Implementation for the defaults button
-end
-
-local function createAcceptButton(f)
-    -- Implementation for the accept button
-end
-
 local LINE_SEGMENT_LENGTH = 575
 local X_START_POINT = 10
 local Y_START_POINT = 10
+
+-- Temporary variables for checkbox states
+local tempDebuggingEnabled = WOWTHREADS_SAVED_VARS.debuggingIsEnabled or false
+local tempDataCollectionEnabled = WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled or false
 
 local function drawLine(f, yPos)
     local lineFrame = CreateFrame("FRAME", nil, f)
@@ -43,7 +33,7 @@ local function drawLine(f, yPos)
     lineFrame:SetSize(LINE_SEGMENT_LENGTH, 2)
     
     local line = lineFrame:CreateLine(nil, "ARTWORK")
-    line:SetColorTexture(.5, .5, .5, 1) -- Grey per https://wow.gamepedia.com/Power_colors
+    line:SetColorTexture(.5, .5, .5, 1) -- Grey
     line:SetThickness(2)
     line:SetStartPoint("LEFT", 0, 0)
     line:SetEndPoint("RIGHT", 0, 0)
@@ -51,41 +41,28 @@ local function drawLine(f, yPos)
 end
 
 local function showExecutionOptions(frame, yPos)
-
-    -- Create check button to toggle strict debugging
+    -- Create check button to toggle debugging
     local debuggingButton = CreateFrame("CheckButton", "Toggle_DebuggingButton", frame, "ChatConfigCheckButtonTemplate")
     debuggingButton:SetPoint("TOPLEFT", 20, yPos)
     debuggingButton.tooltip = L["TOOLTIP_DEBUGGING"]
     _G[debuggingButton:GetName().."Text"]:SetText(L["ENABLE_ERROR_LOGGING"])
-    print( "line 63 debuggingIsEnabled()", thread:isDebuggingEnabled() )
-    debuggingButton:SetChecked( thread:isDebuggingEnabled() )
+    debuggingButton:SetChecked(WOWTHREADS_SAVED_VARS.debuggingIsEnabled)
     debuggingButton:SetScript("OnClick", 
         function(self)
-            local isTrue = self:GetChecked() and true or false
-            enableDebugging = isTrue
-            if isTrue then 
-                thread:enableDebugging()
-            else
-                thread:disableDebugging()
-            end
+            tempDebuggingEnabled = self:GetChecked()
+            -- utils:dbgPrint(addonName .. ": Debugging checkbox toggled - tempDebuggingEnabled: " .. tostring(tempDebuggingEnabled))
         end)
 
     -- Create check button to toggle data collection
     local dataCollectionButton = CreateFrame("CheckButton", "Toggle_DataCollectionButton", frame, "ChatConfigCheckButtonTemplate")
     dataCollectionButton:SetPoint("TOPLEFT", 290, yPos)
     dataCollectionButton.tooltip = L["TOOLTIP_DATA_COLLECTION"]
-    _G[dataCollectionButton:GetName().."Text"]:SetText(L["ENABLE_DATA_COLLECTION"])
-    print( "line 81 dataCollectionIsEnabled()", thread:dataCollectionIsEnabled())
-    dataCollectionButton:SetChecked( thread:dataCollectionIsEnabled() )
+    _G[dataCollectionButton:GetName().."Text"]:SetText(L["Enable Data Collection"])
+    dataCollectionButton:SetChecked(WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled)
     dataCollectionButton:SetScript("OnClick", 
         function(self)
-            local isTrue = self:GetChecked() and true or false
-            enableDataCollection = isTrue
-            if isTrue then
-                thread:enableDataCollection()
-            else
-                thread:disableDataCollection()
-            end
+            tempDataCollectionEnabled = self:GetChecked()
+            -- utils:dbgPrint(addonName .. ": Data collection checkbox toggled - tempDataCollectionEnabled: " .. tostring(tempDataCollectionEnabled))
         end)
 end
 
@@ -108,7 +85,7 @@ local function createOptionsPanel()
     })
     frame:SetBackdropColor(0.0, 0.0, 0.0, 0.85)
 
-    -- The Title Bar & Title
+    -- Title Bar & Title
     frame.titleBar = frame:CreateTexture(nil, "ARTWORK")
     frame.titleBar:SetTexture("Interface\\DialogFrame\\UI-DialogBox-Header")
     frame.titleBar:SetPoint("TOP", 0, 12)
@@ -116,7 +93,7 @@ local function createOptionsPanel()
 
     frame.title = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
     frame.title:SetPoint("TOP", 0, 4)
-    frame.title:SetText(L["OPTIONS_MENUS"])
+    frame.title:SetText("Options Menu")
 
     -- Title text
     frame.text = frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
@@ -144,17 +121,19 @@ local function createOptionsPanel()
     frame.acceptButton:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -8, 8)
     frame.acceptButton:SetScript("OnClick",
         function(self)
-            if enableDebugging then  
-                thread:enableDebugging() 
-            else 
-                thread:disableDebugging() 
+           WOWTHREADS_SAVED_VARS.debuggingIsEnabled = tempDebuggingEnabled
+            if WOWTHREADS_SAVED_VARS.debuggingIsEnabled then
+                thread:enableDebugging()
+            else
+                thread:disableDebugging()
             end
-            if enableDataCollection then 
-                thread:enableDataCollection() 
-            else 
-                thread:disableDataCollection() 
+           WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled = tempDataCollectionEnabled
+            if WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled then
+                thread:enableDataCollection()
+            else
+                thread:disableDataCollection()
             end
-            
+            -- utils:postMsg(addonName .. ": Accept button clicked -WOWTHREADS_SAVED_VARS.debuggingIsEnabled: " .. tostring(WOWTHREADS_SAVED_VARS.debuggingIsEnabled) .. ",WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled: " .. tostring(WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled))
             frame:Hide()
         end)
 
@@ -166,12 +145,17 @@ local function createOptionsPanel()
     frame.dismissButton:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT", 8, 8)
     frame.dismissButton:SetScript("OnClick",
         function(self)
+            -- Reset temporary variables and checkboxes to saved state
+            tempDebuggingEnabled = WOWTHREADS_SAVED_VARS.debuggingIsEnabled
+            tempDataCollectionEnabled = WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled
+            _G["Toggle_DebuggingButton"]:SetChecked( WOWTHREADS_SAVED_VARS.debuggingIsEnabled)
+            _G["Toggle_DataCollectionButton"]:SetChecked(WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled)
+            -- -- utils:dbgPrint(addonName .. ": Dismiss button clicked - Reset toWOWTHREADS_SAVED_VARS.debuggingIsEnabled: " .. tostring(WOWTHREADS_SAVED_VARS.debuggingIsEnabled) .. ",WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled: " .. tostring(WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled))
             frame:Hide()
         end)
 
     return frame
 end
-
 
 local optionsPanel = nil
 
@@ -179,11 +163,20 @@ function options:showOptionsPanel()
     if optionsPanel == nil then
         optionsPanel = createOptionsPanel()
     end
+    -- Ensure checkboxes reflect current saved state
+    _G["Toggle_DebuggingButton"]:SetChecked(WOWTHREADS_SAVED_VARS.debuggingIsEnabled)
+    _G["Toggle_DataCollectionButton"]:SetChecked(WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled)
+    tempDebuggingEnabled = WOWTHREADS_SAVED_VARS.debuggingIsEnabled
+    tempDataCollectionEnabled = WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled
+    -- -- utils:dbgPrint(addonName .. ": Options panel opened -WOWTHREADS_SAVED_VARS.debuggingIsEnabled: " .. tostring(WOWTHREADS_SAVED_VARS.debuggingIsEnabled) .. ",WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled: " .. tostring(WOWTHREADS_SAVED_VARS.dataCollectionIsEnabled))
     optionsPanel:Show()
 end
 
 local function hideOptionsPanel()
-	if optionsPanel then optionsPanel:Hide() end
+    if optionsPanel then
+        optionsPanel:Hide()
+        -- utils:dbgPrint(addonName .. ": Options panel hidden")
+    end
 end
 
 WoWThreads.OptionsPanel.loaded = true
